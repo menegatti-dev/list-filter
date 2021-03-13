6382eafc2e83bd56847b73f6ca6e608178cf40d6
Marlon Menegatti <marlonmenegatt@gmail.com>
2021-03-13 12:08:51 -0300

    return count of odd number

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,19 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list) do
+    list
+    |> convert_number()
+    |> Enum.count(fn element -> rem(element, 2) == 1 end)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp convert_number(list) do
+    list
+    |> Enum.flat_map(fn string ->
+      case Integer.parse(string) do
+        # transform to integer
+        {int, _rest} -> [int]
+        # skip the value
+        :error -> []
+      end
+    end)
   end
 end
