36d1b1bc7758450bc0833a329944348fb6180223
Marlon Menegatti <marlonmenegatt@gmail.com>
2021-03-13 12:13:52 -0300

    add test to ListFilter module

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "return count of odd numbers" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      response = ListFilter.call(list)
+
+      expected_response = 3
+
+      assert expected_response == response
+    end
   end
 end
